93939e07c177916bd768f6d9d8322e7d05decfe3
fadilatunn <fadillanikma895@gmail.com>
2025-12-11 20:05:13 +0700

    feat: tambah komponen Blade yang hilang

---
+++ resources/views/components/primary-button.blade.php
@@ -0,0 +1,3 @@
+<button {{ $attributes->merge(['class' => 'inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-white']) }}>
+    {{ $slot }}
+</button>
